d3550760b59b44139b5f38e295c0e201fc349793
Agamal88 <132816767+Agamal88@users.noreply.github.com>
2025-06-29 20:30:21 +0300

    Added matchmaking package
    package inc. : matchmaking page, profile, after call page
    
    NOTE : call_page.dart IS A PLACEHOLDER

---
+++ lib/screens/matchmaking/call_page.dart
@@ -0,0 +1,16 @@
+import 'package:fluently_frontend/models/user_model.dart';
+import 'package:flutter/cupertino.dart';
+
+class CallPage extends StatefulWidget {
+  const CallPage({super.key, required User user});
+
+  @override
+  State<CallPage> createState() => _CallPageState();
+}
+
+class _CallPageState extends State<CallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
